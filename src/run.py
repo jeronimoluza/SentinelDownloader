3aa1148e710bd30e697621762d66efcb041e0374
Jeronimo Luza <93681172+jeronimoluza@users.noreply.github.com>
2022-03-24 16:44:48 -0400

    no manifest download

--- src/run.py
+++ src/run.py
@@ -12,13 +12,13 @@ def download_pipeline(config):
     logger.info('... DOWNLOAD START ...')
     # downloads urls 
     # download_links(config)
-    download_links(config)
+    #download_links(config)
     # download data
     download_data(config)
 
 
 def run_pipeline():
-    config = get_config('config/config_local.yaml')
+    config = get_config('config/config.yaml')
     path = config['project_dir']
     os.chdir(path)
     print(os.getcwd())
