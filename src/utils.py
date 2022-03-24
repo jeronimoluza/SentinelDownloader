1198629af8b4d7ee416a92eedc1d3721955bbbea
Jeronimo Luza <93681172+jeronimoluza@users.noreply.github.com>
2022-03-24 16:42:14 -0400

    deleted unnecessary functions

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from loguru import logger
 import os
 import pandas as pd
-from netCDF4 import Dataset
 
 
 def get_config(path):
@@ -57,58 +56,6 @@ def get_downloaded_dates(incoming_dir, processed_dir):
     codes = [_get_file_code(x) for x in files if x.startswith('S5P')]
     dates = set([_get_date_from_code(x) for x in codes])
     return list(sorted(dates))
-
-
-def nc_files_list(data_dir):
-    '''
-    Retrieves nc files list'''
-    nc_dir = f'{data_dir}ncfiles/'
-    nfl = list()
-    for file in os.listdir(nc_dir):
-        if 'S5P_OFFL_L2__NO2' in file:
-            nfl.append(file)
-    
-    return nfl
-
-def nc_file_integrity(data_dir):
-    '''
-    Checks integrity of nc files by opening them
-    Returns
-    -------
-    list :
-        files that are complete and ready for reading'''
-    
-    txt_location = f'{data_dir}urls/downloaded/downloaded.txt'
-    good_files = list()
-    nc_dir = f'{data_dir}ncfiles/'
-    for file in nc_files_list(data_dir):
-        try:
-            Dataset(f'{nc_dir}{file}', 'r')
-            good_files.append(file)
-        except:
-            os.remove(f'{nc_dir}{file}')
-            continue
-    logger.debug(f'... {len(good_files)} DOWNLOADED AND CHECKED FILES ...')
-    with open(txt_location, 'w') as f:
-        for file in good_files:
-            f.write(file + "\n")
-        f.close()
-
-
-def file_checker(data_dir):
-    """Checks for downloaded files and writes list 
-    to avoid downloading them again
-    """
-    logger.debug(f'... CHECKING FILES DOWNLOAD PROGRESS ...')
-    nc_file_integrity(data_dir)
-    
-    
-    nc_files = nc_files_list(data_dir)
-    downloaded = get_downloaded_list(data_dir)
-    
-    files_to_skip = [file for file in nc_files if file in downloaded]
-    
-    return files_to_skip
 
 
 
